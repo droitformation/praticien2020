22d220bf6b51ab96cdd137bd0ad5bfa9b39fdb77
Cindy Leschaud <cindy.leschaud@gmail.com>
2020-10-05 13:18:04 +0200

    add search

--- resources/views/arrets/index.blade.php
+++ resources/views/arrets/index.blade.php
@@ -3,51 +3,7 @@
     <div class="container">
 
         <div class="py-5">
-            <div class="row">
-                <div class="col-lg-12">
-
-                    <h3>Recherche</h3>
-
-                    <div class="row">
-                        <div class="col-lg-4">
-                            <form action="{{ secure_url('searchTerm') }}" method="POST">@csrf
-                                {!! Honeypot::generate('my_name', 'my_time') !!}
-                                <div class="input-group mb-3">
-                                    <input class="form-control" name="term" type="text" placeholder="Termes...">
-                                    <div class="input-group-append">
-                                        <button class="btn btn-secondary" type="submit">Ok</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="col-lg-8">
-                            <form action="{{ secure_url('searchLoi') }}" method="POST">@csrf
-                                {!! Honeypot::generate('my_name', 'my_time') !!}
-                                <div class="input-group mb-3">
-                                    <input class="form-control" name="params[article]" type="text" placeholder="Article">
-                                    <input class="form-control" name="params[loi]" type="text" placeholder="Loi">
-                                    <input class="form-control" name="params[alinea]" type="text" placeholder="Alinéa">
-                                    <input class="form-control" name="params[chiffre]" type="text" placeholder="Chiffre">
-                                    <input class="form-control" name="params[lettre]" type="text" placeholder="Lettre">
-                                    @if(!empty($editions))
-                                        <select class="custom-select" name="year">
-                                            <option selected>Édition</option>
-                                            @foreach($editions as $edition)
-                                                <option value="{{ $edition }}">{{ $edition }}</option>
-                                            @endforeach
-                                        </select>
-                                    @endif
-                                    <div class="input-group-append">
-                                        <button class="btn btn-secondary" type="submit">Ok</button>
-                                    </div>
-                                </div>
-                            </form>
-
-
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @include('arrets.partials.search')
             <hr>
             <div class="row">
                 <div class="col-md-12">
--- resources/views/arrets/results.blade.php
+++ resources/views/arrets/results.blade.php
@@ -2,8 +2,19 @@
 @section('content')
 
     <div class="container">
+
+       <div class="pt-5">
+           <a class="btn btn-sm btn-search" data-toggle="collapse" href="#collapseSearch" role="button" aria-expanded="false" aria-controls="collapseSearch">
+              <i class="fas fa-redo"></i> &nbsp;Nouvelle recherche
+           </a>
+           <div class="collapse mt-4" id="collapseSearch">
+               @include('arrets.partials.search')
+               <hr>
+           </div>
+       </div>
+
         <div class="row">
-            <div class="col-lg-12 py-5">
+            <div class="col-lg-12 pt-1 pb-2">
                 <div class="block-title">
                     <h3>Résultats pour {{ $term ?? frontendParams($params) }}</h3>
                 </div>
