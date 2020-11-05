0ceb67e336f77a43902dcee425fc0c5848920034
Cindy Leschaud <cindy.leschaud@gmail.com>
2020-11-05 11:15:50 +0100

    add old in form register

--- resources/views/pages/access.blade.php
+++ resources/views/pages/access.blade.php
@@ -17,13 +17,13 @@
                                 <div class="col-lg-12">
                                     <div class="form-wrapper">
                                         <i class="fas fa-tag"></i>
-                                        <input type="text" class="code-input" name="code" required placeholder="Code d'accès">
+                                        <input type="text" class="code-input" name="code" value="{{ old('code') }}" required placeholder="Code d'accès">
                                     </div>
                                 </div>
                             </div>
                             <div class="row mb-1">
                                 <div class="col-lg-12">
-                                    <input type="email" name="email" required placeholder="Email">
+                                    <input type="email" name="email" required placeholder="Email" value="{{ old('email') }}" >
                                 </div>
                                 <div class="col-lg-12">
                                     <input type="password" name="password" required placeholder="Mot de passe">
@@ -35,19 +35,19 @@
                             <div class="divider div-transparent div-arrow-down"></div>
                             <div class="row">
                                 <div class="col-lg-6">
-                                    <input type="text" name="first_name" required placeholder="Prénom">
+                                    <input type="text" name="first_name" required placeholder="Prénom" value="{{ old('first_name') }}" >
                                 </div>
                                 <div class="col-lg-6">
-                                    <input type="text" name="last_name" required placeholder="Nom">
+                                    <input type="text" name="last_name" required placeholder="Nom" value="{{ old('last_name') }}" >
                                 </div>
                                 <div class="col-lg-12">
-                                    <input type="text" name="adresse" required placeholder="Adresse">
+                                    <input type="text" name="adresse" required placeholder="Adresse" value="{{ old('adresse') }}" >
                                 </div>
                                 <div class="col-lg-6">
-                                    <input type="text" name="npa" required placeholder="NPA">
+                                    <input type="text" name="npa" required placeholder="NPA" value="{{ old('npa') }}" >
                                 </div>
                                 <div class="col-lg-6">
-                                    <input type="text" name="ville" required placeholder="Ville">
+                                    <input type="text" name="ville" required placeholder="Ville" value="{{ old('ville') }}" >
                                 </div>
                                 <div class="col-lg-12">
                                     <button class="thm-btn contact-one__btn" type="submit">Envoyer</button>
