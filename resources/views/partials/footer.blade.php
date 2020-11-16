83156bb6e0ed342bbb286c34260e0fbc55a5dca4
Cindy Leschaud <cindy.leschaud@gmail.com>
2020-11-16 08:03:03 +0100

    fix footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="site-footer__bottom">
         <div class="container">
             <div class="inner-container">
-                <p>&copy; Copyright {{ date('Y') }} Droit praticien. All Rights Reserved</p>
+                <p>&copy; Copyright {{ date('Y') }} Droit pour le praticien. All Rights Reserved</p>
                 <a href="index.html" class="site-footer__bottom-logo">
                     <img class="footer-unine" src="{{ secure_asset('images/unine.svg') }}" alt="">
                 </a>
